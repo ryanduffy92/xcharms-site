0f53335ab2fe441be398ce8aa4e6fe3faf278d1c
Ryan Duffy <r.duffy@bristol.ac.uk>
2020-11-22 19:49:44 +0000

    namespaced urls

--- clusters/urls.py
+++ clusters/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'clusters'
 urlpatterns = [
 		path('', views.table, name='table'),
-		path('<str:root>/', views.analysis_page, name='analysis page'),
+		path('<str:root>/', views.analysis_page, name='analysis_page'),
 ]
--- clusters/views.py
+++ clusters/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Cluster
@@ -7,7 +7,7 @@ def table(request):
 	all_clusters = Cluster.objects.order_by('z')
 	context = {'all_clusters': all_clusters}
 	return render(request, 'clusters/table.html', context)
-	#return HttpResponse("This is where the table of clusters goes.")
 
 def analysis_page(request, root):
-	return HttpResponse("This will display the cluster analysis of the cluster with root {}.".format(root))
+	cluster = get_object_or_404(Cluster, pk=root)
+	return render(request, 'clusters/analysis_page.html', {'cluster': cluster})
