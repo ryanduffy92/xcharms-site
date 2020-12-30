557ac058678e3b9c403b1ccc9b520a08297b7c8a
Ryan Duffy <r.duffy@bristol.ac.uk>
2020-12-30 17:33:15 +0000

    Added Obs class to models to describe observation data for each cluster. Added this to display in admin.py. Made json of obs data for macs and added to fixtures, populated Obs database using this.

--- clusters/admin.py
+++ clusters/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Cluster
+from .models import Cluster, Obs
 
 admin.site.register(Cluster)
+admin.site.register(Obs)
--- clusters/models.py
+++ clusters/models.py
@@ -5,5 +5,15 @@ class Cluster(models.Model):
 	z = models.DecimalField(verbose_name="Redshift", max_digits=8, decimal_places=4)
 	ra = models.DecimalField(verbose_name="RA", max_digits=10, decimal_places=4)
 	dec = models.DecimalField(verbose_name="Dec", max_digits=10, decimal_places=4)
-	goodtime = models.DecimalField(verbose_name="Clean exptime (s)",max_digits=20, decimal_places=2)
+	goodtime = models.DecimalField(verbose_name="Clean exptime (s)", max_digits=20, decimal_places=2)
 	catalogue = models.CharField(max_length=20)
+
+class Obs(models.Model):
+	cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+	obsid = models.IntegerField(verbose_name="ObsID")
+	instrument = models.CharField(verbose_name="Detector", max_length=6)
+	detnam = models.CharField(verbose_name="Detector CCDs", max_length=15)
+	expgoodtime = models.DecimalField(verbose_name="Expected length (ks)", max_digits=20, decimal_places=2)
+	goodtime = models.DecimalField(verbose_name="Good time (s)", max_digits=20, decimal_places=2)
+	badtime = models.DecimalField(verbose_name="Bad time (s)", max_digits=20, decimal_places=2)
+	pubdate = models.DateField(verbose_name="Public Date")
